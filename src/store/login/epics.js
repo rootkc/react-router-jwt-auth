068cbb32c532d2989ae2e9e0109a386f9309571a
rootkc <info@kennethchristensen.dk>
2017-12-01 22:42:38 +0100

    use deconstruct in login epic

--- src/store/login/epics.js
+++ src/store/login/epics.js
@@ -6,10 +6,7 @@ const loginUserEpic = action$ =>
   action$.ofType(LOGIN_USER)
     .mergeMap(action =>
       api('/login', 'POST', { user: action.payload })
-        .do(({ response: { data } }) => localStorage.saveState(data.token, data.id))
-        .map(({ response: { data } }) => loginUserFufilled({
-          token: data.token,
-          id: data.id,
-        })))
+        .do(({ response: { data: { token, id } } }) => localStorage.saveState(token, id))
+        .map(({ response: { data: { token, id } } }) => loginUserFufilled({ token, id })))
 
 export default loginUserEpic
