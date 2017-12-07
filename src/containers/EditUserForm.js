85b889924d7f56267edf98769109c3f1bf3983af
rootkc <info@kennethchristensen.dk>
2017-12-07 20:33:41 +0100

    add initial value on edit username

--- src/containers/EditUserForm.js
+++ src/containers/EditUserForm.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { connect } from 'react-redux'
 import { reduxForm } from 'redux-form'
 import { UserForm } from 'components'
 import { editUser } from 'store/actions'
@@ -7,8 +8,14 @@ const EditUserFormContainer = props => <UserForm title="Edit" label="edit" {...p
 
 const onSubmit = (data, dispatch) => dispatch(editUser(data))
 
-export default reduxForm({
+const mapStateToProps = state => ({
+  initialValues: { username: state.user.username },
+})
+
+const formEditUserFormContainer = reduxForm({
   form: 'EditUserForm',
   destroyOnUnmount: false,
   onSubmit,
 })(EditUserFormContainer)
+
+export default connect(mapStateToProps, null)(formEditUserFormContainer)
